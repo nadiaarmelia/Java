581acb6e19126695a6e46035873fb1bad84fa47a
Nadia Armelia Amanda <nadiaarmelia.na@gmail.com>
2020-01-12 21:21:49 +0700

    Add files via upload

---
+++ SekuensialMenukar2Bilangan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ACER
+ */
+public class SekuensialMenukar2Bilangan {
+    public static void main(String[] args){
+        int a=7, b=9, c;
+        
+        System.out.println("Sebelum Ditukar");
+        System.out.println("a ="+a+ " b ="+b);
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("Sesudah Ditukar");
+        System.out.println("a ="+a+ " b="+b);
+        
+    }
+}
