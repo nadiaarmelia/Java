c851ee3b8db42f6a4c8d47c8e9d9796cba2d4269
Nadia Armelia Amanda <nadiaarmelia.na@gmail.com>
2020-01-12 21:32:02 +0700

    Add files via upload

---
+++ SekuensialDataDiri.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ACER
+ */
+public class SekuensialDataDiri {
+    public static void main(String[] args){
+        String nama, alamat;
+        int usia;
+        double tinggi;
+        
+        nama = "Nadia Armelia Amanda";
+        alamat = "Jalan Pengadegan Selatan VII";
+        usia = 18;
+        tinggi = 162.5;
+        
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Usia: " + usia + " tahun");
+        System.out.println("Tinggi: " + tinggi + " cm");
+    }
+}
